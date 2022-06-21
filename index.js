f746882a71366da5053d783f6bc096b80d9ab355
CarlosMurrieta12 <emilimurrieta@icloud.com>
2022-06-21 01:39:28 -0700

    First Commit

---
+++ index.js
@@ -0,0 +1,38 @@
+var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+     {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+     {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["JavaScript", "Gaming", "Foxes"]
+    },
+];
+
+/* Noticed that using bracket notation to find a property in the object is better than dot
+notation. Ex = contacts[i][prop] */
+function lookUpProfile(name, prop) {
+    
+}
+
+/* Change the values to test your function */
+var data = lookUpProfile("Sherlock", "number");
+console.log(data);
+/* Line below shows how to find objects in an array */
+console.log(contacts[2]);
